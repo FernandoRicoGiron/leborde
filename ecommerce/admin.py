38e36391c38e01456bf2c49fb374399ac518649c
Rico <riicoo28@gmail.com>
2019-03-14 12:08:00 -0600

    sds

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from .models import *
 
+class Productos(admin.ModelAdmin):
+	list_display = ["id","nombre"]
+	list_display_links = ["id","nombre"]
+	search_fields = ['nombre']
+	filter_horizontal = ['imagenes']
+	 
+	class Meta:
+		model = Producto
+
 # Tienda
 admin.site.register(Imagen)
 admin.site.register(Categoria)
 admin.site.register(Talla)
-admin.site.register(Producto)
+admin.site.register(Producto, Productos)
 admin.site.register(Ficha_Tecnica)
 admin.site.register(Coleccion)
 admin.site.register(Envio)
