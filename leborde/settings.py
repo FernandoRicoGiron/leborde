7b107ecc3d37defc66175c6ccc5f4f50b511a32b
Rico <riicoo28@gmail.com>
2019-07-30 14:40:29 -0500

    paginacion

--- leborde/settings.py
+++ leborde/settings.py
@@ -87,22 +87,22 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tuxmapa_istmena',
-        'USER': 'tuxmapa',
-        'PASSWORD': 'rockmax28',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     #     'NAME': 'tuxmapa_istmena',
     #     'USER': 'tuxmapa',
-    #     'PASSWORD': 'i0cj6y339JMxHb2Z',
+    #     'PASSWORD': 'rockmax28',
     #     'HOST': 'localhost',
     #     'PORT': '',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tuxmapa_istmena',
+        'USER': 'tuxmapa',
+        'PASSWORD': 'i0cj6y339JMxHb2Z',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
 
 
