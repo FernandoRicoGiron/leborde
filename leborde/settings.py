65d532c678121f66097e2b5aaff653b938bdcce2
Rico <riicoo28@gmail.com>
2019-07-30 14:44:03 -0500

    debug

--- leborde/settings.py
+++ leborde/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uhajcswr-7!mre&h#$t8jpytbu@9g8!1kt)q7el!^0@zu6(4ue'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
